6b3216830fd1bfb44eb6b2516617c4593a8828be
Son Truong <sntruong12@gmail.com>
2019-02-04 16:14:16 -0600

    update pseudocode on README.md and main.js

--- README.md
+++ README.md
@@ -21,15 +21,20 @@ HTML Canvas, JavaScript.
 
 1. The start button will initialize the game.
 - Fade in audio of instrumental Travis Scott music.
-2. On the 10 cells wide by 20 cells high game board, one tetromino will descend from the top row down to the bottom of the game board.
+
+2. Create the game board
 - The game board will contain random opaque background images of Travis Scott.
+
+3. Draw a tetromino that will descend from the top row down to the bottom of the game board. 
+
 - We need to design all 7 tetromino pieces.
+- 
 - The tetromino will drop one row every 1 second.
 - Each piece can move left, right, or down on the board.
 - Each piece cannot move beyond the left and right wall of the game board.
 - We need to lock the pieces in place once they reach the bottom of the board or touch the top of anoother piece. 
 - Once the game piece is locked, that should update the gameboard and spawn a new random tetromino at the top. 
-3. When all ten cells in a single row or mulitple rows is occupied, update the gameboard to clear those rows that are filled.
+4. When all ten cells in a single row or mulitple rows is occupied, update the gameboard to clear those rows that are filled.
 - If there are cells occupied on any rows above a cleared row, those rows should shift down and fill in the cleared rows.
 - If a row is cleared, add a sound bite of Travis Scott's adlibs will play once.
 - When you clear a row or multiple rows the score will be updated based on the following table.
@@ -41,7 +46,7 @@ Rows cleared | Points awarded
 3 | 600
 4 | 800
 
-4. End the game when any tetromino piece is placed outside the top row of the game board.
+5. End the game when any tetromino piece is placed outside the top row of the game board.
 - Display some image and highlight the score.
 - Also display a reply button that will reinitialize the game for them.
 
--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 /*----- constants -----*/ 
 
-// define how many rows and columsn the gameboard contains
+// define how many rows and columns the gameboard contains - 200 cells total.
 const ROW = 20;
 const COL = 10;
+
+//SQ is for square which will represent our individual cells on the board.
+const SQ = 30;
 
 // define an empty sqare in the board
 const VACANT = '#FFFFFF';
@@ -12,7 +15,9 @@ const VACANT = '#FFFFFF';
 let board;
 
 /*----- cached element references -----*/ 
-
+const canvas = document.querySelector('canvas');
+// this is a method on the canvas obj that allows us to draw on the canvas
+const context = canvas.getContext('2d');
 
 /*----- event listeners -----*/ 
 
@@ -20,13 +25,14 @@ let board;
 
 // 1. The start button will initialize the game.
 // - Fade in audio of instrumental Travis Scott music.
-init ();
+init();
+
 function init() {
   board = [];
   createBoard();
 }
 
-// 2. On the 10 cells wide by 20 cells high game board, one tetromino will descend from the top row down to the bottom of the game board.
+// 2. Create the game board
 function createBoard () {
 //creates 20 rows on the board
   for(let r = 0; r < ROW; r++) {
@@ -41,12 +47,18 @@ function createBoard () {
 }
 
 // - The game board will contain random opaque background images of Travis Scott.
+
+//  Draw a tetromino that will descend from the top row down to the bottom of the game board. 
+
+
 // - We need to design all 7 tetromino pieces.
 // - The tetromino will drop one row every 1 second.
 // - Each piece can move left, right, or down on the board.
 // - Each piece cannot move beyond the left and right wall of the game board.
 // - We need to lock the pieces in place once they reach the bottom of the board or touch the top of anoother piece. 
 // - Once the game piece is locked, that should update the gameboard and spawn a new random tetromino at the top. 
+
+
 // 3. When all ten cells in a single row or mulitple rows is occupied, update the gameboard to clear those rows that are filled.
 // - If there are cells occupied on any rows above a cleared row, those rows should shift down and fill in the cleared rows.
 // - If a row is cleared, add a sound bite of Travis Scott's adlibs will play once.
